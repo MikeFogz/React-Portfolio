9c584ef5c4a879b18963981818c8fb713eef67f0
MikeFogz <MikeFogz@gmail.com>
2021-03-05 01:28:11 -0800

    Added footer component & code

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <footer class="fixed-bottom footer text-center bg-info">
+            <small>Copyright &copy; Mike Fogarty's Portfolio</small>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
